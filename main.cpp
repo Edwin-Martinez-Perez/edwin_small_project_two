01be9b1628fb35aa876665183a3222865af84e56
Edwin <eamartinezperez@noctrl.edu>
2022-04-06 15:29:24 -0500

    Creation

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Edwin Martinez
+//CSCE 306; Spring 2022
+//main.cpp
+
+//Implementation of two-file classes (.h and .cpp) for each,
+// three global arrays (static) with three corresponding global functions (static)
+// to access the data in those arrays, and a driver file.
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
